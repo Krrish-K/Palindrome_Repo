2c14c04a879ea56b799ceec602e52974a44ca032
Harikrishnan V <krishnahare.v@gmail.com>
2019-10-01 23:16:31 +0530

    Edited and Added code for Palindrome program

--- palindrome.c
+++ palindrome.c
@@ -1,1 +1,17 @@
-
+#include<stdio.h>
+void main()
+{int n,a,r,d;
+printf("Enter the number:");
+scanf("%d",&n);
+r=0;
+a=n;
+while(n>0)
+{d=n%10;
+r=(10*r)+d;
+n=n/10;
+}
+if(a==r)
+printf("%d is a palindrome",a);
+else
+printf("%d is not a palindrome",a);
+}
